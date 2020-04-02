b4f75a3a2fd340c255c3bfe2841858dd6ff40d83
Your Name <you@example.com>
2020-04-02 19:28:37 +0800

    爬取商品信息

---
+++ TGscrapy/TGscrapy/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TgscrapyItem(scrapy.Item):
+    # define the fields for your item here like:
+    shopping_classify = scrapy.Field()#商品的种类
+    shopping_name = scrapy.Field()#商品的名字
+    shopping_jiage = scrapy.Field()#商品的价格
+    shopping_info = scrapy.Field()#商品的详情
+    shopping_sv = scrapy.Field()#商品的销量
+    shopping_photo = scrapy.Field()#商品的样式
